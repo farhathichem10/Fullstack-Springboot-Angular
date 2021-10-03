a3e62a624cb67bfd84f81eb440ffbf1c7688d72c
farhathichem10 <hichem.farhat95@gmail.com>
2021-10-03 11:20:43 +0100

    bootstrap

--- src/main/java/com/sip/Ams/controller/EtudiantController.java
+++ src/main/java/com/sip/Ams/controller/EtudiantController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sip.Ams.entities.Etudiant;
-
+@RequestMapping("/etudiant")
 @Controller
 public class EtudiantController {
 List<Etudiant> etudiant = new ArrayList<>();
